c383f51e2f055c6ad2f4c514ffc4b65ac4220a7a
fmunin <frm@muninsolutions.com>
2025-07-15 21:49:30 -0400

    Update backend login for module 4

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+//updates from Module 4
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+//end of module 4 updates
+
 import './LoginPage.css';
 
 function LoginPage() {
@@ -6,10 +12,61 @@ function LoginPage() {
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    //updates from module 4
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+      if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+      }
+    }, [navigate]);
+
+    //end of module 4 updates
+
+
     // insert code here to create handleLogin function and include console.log
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
-    }
+      e.preventDefault();
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            //Step 1 - Task 7
+            method: 'POST',
+            //Step 1 - Task 8
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+        //Step 1 - Task 9
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+        //Step 2: Task 1
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            //Step 2: Task 2
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            //Step 2: Task 3
+          setIsLoggedIn(true);
+            //Step 2: Task 4
+          navigate('/app');
+        } else {
+            //Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+      }
         return (
       <div className="container mt-5">
         <div className="row justify-content-center">
